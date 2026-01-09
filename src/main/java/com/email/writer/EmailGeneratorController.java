c40d714df8868abce03f20e1fd87f2795c4626c2
Akash9634 <akashkush2020@gmail.com>
2026-01-09 23:54:46 +0530

    final touch

--- src/main/java/com/email/writer/EmailGeneratorController.java
+++ src/main/java/com/email/writer/EmailGeneratorController.java
@@ -1,21 +1,21 @@
 package com.email.writer;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/email")
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
+
 
     private final EmailGeneratorService emailGeneratorService;
     public EmailGeneratorController(EmailGeneratorService emailGeneratorService){
         this.emailGeneratorService=emailGeneratorService;
     }
 
+    @CrossOrigin(origins="*")
     @PostMapping("/generate")
     public Mono<String> generateEmail(@RequestBody EmailRequest emailRequest){
         return emailGeneratorService.generateEmailReply(emailRequest);
